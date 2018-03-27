793811afbe539c6b54f806a9aabd59de270dafd7
Matt Innes <matt@madeye.com>
2018-03-27 12:15:09 +0100

    Removed creation of pgcrypto to fix boot error in sample service

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,3 @@ CREATE USER actionsvc PASSWORD 'actionsvc'
   NOSUPERUSER NOCREATEDB NOCREATEROLE NOREPLICATION INHERIT LOGIN;
 
 CREATE SCHEMA action;
-
--- create postgres extension to allow generation of v4 UUID
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
