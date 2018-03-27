a1174543f9e8f172db2767f5c40613b95e906296
Joseph Walton <jwgwalton@gmail.com>
2018-03-27 08:25:37 +0100

    Checks the casegroupstatus isn't completed/completedbyphone before sending actions

--- src/main/java/uk/gov/ons/ctp/response/action/service/impl/ActionRequestValidator.java
+++ src/main/java/uk/gov/ons/ctp/response/action/service/impl/ActionRequestValidator.java
@@ -17,69 +17,19 @@ public class ActionRequestValidator {
 
     /**
      * Validates whether the ActionRequest should be sent to a handler service.
-     * Contains the business logic for deciding whether the recipient is to receive an email or letter,
-     * dependent on the status of their account and the response.
-     * @param actionType
+     * Checks if the CaseGroupStatus is completed, this stops any actions beings sent for BI
+     * Cases where other BI's have completed the survey
      * @param actionRequest
      * @return isValid
      */
-    public boolean validate(final ActionType actionType, final ActionRequest actionRequest) {
-        String handler = actionType.getHandler();
-        // Completed no action required
+    public boolean validate(final ActionRequest actionRequest) {
+        boolean isValid = true;
+
         if (caseCompleted(actionRequest) || caseCompletedByPhone(actionRequest)) {
-            return false;
+            isValid = false;
         }
 
-        if (isEmail(handler) && hasActiveRespondent(actionRequest) && enrolmentEnabled(actionRequest)) {
-            return validateEmail(actionType, actionRequest);
-        }
-
-        if (isLetter(handler)) {
-            return validateLetter(actionRequest);
-        }
-
-        log.info("Invalid action request: handler = {} : respondentStatus {} :  enrolmentStatus {} : actionTypePK {}",
-                handler, actionRequest.getCaseGroupStatus(), actionRequest.getEnrolmentStatus(),
-                actionType.getActionTypePK());
-
-        return false;
-    }
-
-
-    private boolean validateEmail(final ActionType actionType, final ActionRequest actionRequest) {
-        if (isNotificationEmail(actionType)) {
-            return true;
-        }
-        if (isReminderEmail(actionType) && (caseInProgress(actionRequest) || caseNotStarted(actionRequest))) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean validateLetter(final ActionRequest actionRequest) {
-        if (hasNoRespondent(actionRequest) && hasNoEnrolment(actionRequest) && caseNotStarted(actionRequest)) {
-            return true;
-        }
-        if (enrolmentPending(actionRequest) && hasCreatedRespondent(actionRequest) && caseNotStarted(actionRequest)) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isLetter(final String handler) {
-        return ACTIONEXPORTER.equalsIgnoreCase(handler);
-    }
-
-    private boolean isEmail(final String handler) {
-        return NOTIFYGATEWAY.equalsIgnoreCase(handler);
-    }
-
-    private boolean isReminderEmail(final ActionType actionType) {
-        return actionType.getActionTypePK() != null && actionType.getActionTypePK().equals(3);
-    }
-
-    private boolean isNotificationEmail(final ActionType actionType) {
-        return actionType.getActionTypePK() != null && actionType.getActionTypePK().equals(4);
+        return isValid;
     }
 
     private boolean caseCompleted(final ActionRequest actionRequest) {
@@ -88,38 +38,6 @@ public class ActionRequestValidator {
 
     private boolean caseCompletedByPhone(final ActionRequest actionRequest) {
         return CaseGroupStatus.COMPLETEDBYPHONE.toString().equalsIgnoreCase(actionRequest.getCaseGroupStatus());
-    }
-
-    private boolean caseInProgress(final ActionRequest actionRequest) {
-        return CaseGroupStatus.INPROGRESS.toString().equalsIgnoreCase(actionRequest.getCaseGroupStatus());
-    }
-
-    private boolean caseNotStarted(final ActionRequest actionRequest) {
-        return CaseGroupStatus.NOTSTARTED.toString().equalsIgnoreCase(actionRequest.getCaseGroupStatus());
-    }
-
-    private boolean enrolmentPending(final ActionRequest actionRequest) {
-        return ActionProcessingServiceImpl.PENDING.equalsIgnoreCase(actionRequest.getEnrolmentStatus());
-    }
-
-    private boolean enrolmentEnabled(final ActionRequest actionRequest) {
-        return ActionProcessingServiceImpl.ENABLED.equalsIgnoreCase(actionRequest.getEnrolmentStatus());
-    }
-
-    private boolean hasNoEnrolment(final ActionRequest actionRequest) {
-        return actionRequest.getEnrolmentStatus() == null;
-    }
-
-    private boolean hasActiveRespondent(final ActionRequest actionRequest) {
-        return RESPONDENTACTIVE.equalsIgnoreCase(actionRequest.getRespondentStatus());
-    }
-
-    private boolean hasCreatedRespondent(final ActionRequest actionRequest) {
-        return RESPONDENTCREATED.equalsIgnoreCase(actionRequest.getRespondentStatus());
-    }
-
-    private boolean hasNoRespondent(final ActionRequest actionRequest) {
-        return actionRequest.getRespondentStatus() == null;
     }
 
 }
--- src/test/java/uk/gov/ons/ctp/response/action/service/impl/ActionRequestValidatorTest.java
+++ src/test/java/uk/gov/ons/ctp/response/action/service/impl/ActionRequestValidatorTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
-import uk.gov.ons.ctp.response.action.domain.model.ActionType;
 import uk.gov.ons.ctp.response.action.message.instruction.ActionRequest;
 import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupStatus;
 
@@ -17,195 +16,25 @@ public class ActionRequestValidatorTest {
 
     @Test
     public void testValidNotificationLetter() {
-        ActionType actionType = buildNotificationLetterActionType();
         ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.NOTSTARTED.toString())
                 .withEnrolmentStatus(ActionProcessingServiceImpl.PENDING)
                 .withRespondentStatus(ActionRequestValidator.RESPONDENTCREATED).build();
 
-        assertTrue(validator.validate(actionType, actionRequest));
+        assertTrue(validator.validate(actionRequest));
     }
 
     @Test
     public void testInvalidNotificationLetterCaseCompleted() {
-        ActionType actionType = buildNotificationLetterActionType();
         ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.COMPLETE.toString()).build();
 
-        assertFalse(validator.validate(actionType, actionRequest));
+        assertFalse(validator.validate(actionRequest));
     }
 
     @Test
     public void testInvalidNotificationLetterCaseCompletedByPhone() {
-        ActionType actionType = buildNotificationLetterActionType();
         ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.COMPLETEDBYPHONE.toString()).build();
 
-        assertFalse(validator.validate(actionType, actionRequest));
+        assertFalse(validator.validate(actionRequest));
     }
 
-    @Test
-    public void testInvalidNotificationLetterEnrolmentNotPending() {
-        ActionType actionType = buildNotificationLetterActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withEnrolmentStatus(ActionProcessingServiceImpl.ENABLED)
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTCREATED).build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testInvalidNotifidcationLetterRespondentNotCreated() {
-        ActionType actionType = buildNotificationLetterActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withEnrolmentStatus(ActionProcessingServiceImpl.PENDING)
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTACTIVE).build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-
-    @Test
-    public void testValidReminderLetterPendingEnrolment() {
-        ActionType actionType = buildReminderLetterActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.NOTSTARTED.toString())
-                .withEnrolmentStatus(ActionProcessingServiceImpl.PENDING)
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTCREATED).build();
-
-        assertTrue(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testValidReminderLetterNoActivity() {
-        ActionType actionType = buildReminderLetterActionType();
-        ActionRequest actionRequest = ActionRequest.builder()
-                .withCaseGroupStatus(CaseGroupStatus.NOTSTARTED.toString()).build();
-
-        assertTrue(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testInvalidReminderLetterInvalidEnrolment() {
-        ActionType actionType = buildReminderLetterActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withEnrolmentStatus("SUSPENDED")
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTCREATED)
-                .build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testInvalidReminderLetterInvalidRespondentStatus() {
-        ActionType actionType = buildReminderLetterActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTACTIVE)
-                .withEnrolmentStatus(ActionProcessingServiceImpl.PENDING).build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testValidReminderEmailCaseInProgress() {
-        ActionType actionType = buildReminderEmailActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withEnrolmentStatus(ActionProcessingServiceImpl.ENABLED)
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTACTIVE).build();
-
-        assertTrue(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testValidReminderEmailCaseNotStarted() {
-        ActionType actionType = buildReminderEmailActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.NOTSTARTED.toString())
-                .withEnrolmentStatus(ActionProcessingServiceImpl.ENABLED)
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTACTIVE).build();
-
-        assertTrue(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testInvalidReminderEmailInvalidRespondentStatus() {
-        ActionType actionType = buildReminderEmailActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withEnrolmentStatus(ActionProcessingServiceImpl.ENABLED)
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTCREATED).build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testInvalidRemindEmailNullEnromentStatus() {
-        ActionType actionType = buildReminderEmailActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTCREATED).build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testInvalidReminderEmailInvalidEnrolment() {
-        ActionType actionType = buildReminderEmailActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.NOTSTARTED.toString())
-                .withEnrolmentStatus("SUSPENDED")
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTACTIVE).build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testValidNotificationEmail() {
-        ActionType actionType = buildNotificationEmailActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTACTIVE)
-                .withEnrolmentStatus(ActionProcessingServiceImpl.ENABLED)
-                .build();
-
-        assertTrue(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testInvalidNotificationEmailInvalidEnrolment() {
-        ActionType actionType = buildNotificationEmailActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTACTIVE)
-                .withEnrolmentStatus(ActionProcessingServiceImpl.PENDING)
-                .build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testInvalidNotificationEmailInvalidRespondentStatus() {
-        ActionType actionType = buildNotificationEmailActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withEnrolmentStatus(ActionProcessingServiceImpl.ENABLED)
-                .withRespondentStatus(ActionRequestValidator.RESPONDENTCREATED).build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-    @Test
-    public void testInvalidNotificationEmailNullRespondentStatus() {
-        ActionType actionType = buildNotificationEmailActionType();
-        ActionRequest actionRequest = ActionRequest.builder().withCaseGroupStatus(CaseGroupStatus.INPROGRESS.toString())
-                .withEnrolmentStatus(ActionProcessingServiceImpl.ENABLED).build();
-
-        assertFalse(validator.validate(actionType, actionRequest));
-    }
-
-
-    private ActionType buildNotificationLetterActionType() {
-        return ActionType.builder().actionTypePK(1).name("BSNOT").handler(ActionRequestValidator.ACTIONEXPORTER).build();
-    }
-
-    private ActionType buildReminderLetterActionType() {
-        return ActionType.builder().actionTypePK(2).name("BSREM").handler(ActionRequestValidator.ACTIONEXPORTER).build();
-    }
-
-    private ActionType buildReminderEmailActionType() {
-        return ActionType.builder().actionTypePK(3).name("BSNE").handler(ActionRequestValidator.NOTIFYGATEWAY).build();
-    }
-
-    private ActionType buildNotificationEmailActionType() {
-        return ActionType.builder().actionTypePK(4).name("UNKNOWN").handler(ActionRequestValidator.NOTIFYGATEWAY).build();
-    }
 }
