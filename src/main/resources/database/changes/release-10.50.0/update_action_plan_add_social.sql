053af5dce66e8bd7595671ed6a14a9cdeb257db0
Emilio Ward <emilioward@hotmail.co.uk>
2018-06-05 11:46:36 +0100

    fix database errors

--- src/main/resources/database/changes/release-10.50.0/update_action_plan_add_social.sql
+++ src/main/resources/database/changes/release-10.50.0/update_action_plan_add_social.sql
@@ -1,5 +1,5 @@
 INSERT INTO action.actiontype
 VALUES
-(4, SOCIALNOT, Enrolment Invitation Letter, Printer, false, false),
-(5, SOCIALREM, Enrolment Reminder Letter, Printer, false, false),
-(6, SOCIALSNE, Survey Reminder Notification, Notify, false, false);
+(4, 'SOCIALNOT', 'Enrolment Invitation Letter', 'Printer', 'n', 'n'),
+(5, 'SOCIALREM', 'Enrolment Reminder Letter', 'Printer', 'n', 'n'),
+(6, 'SOCIALSNE', 'Survey Reminder Notification', 'Notify', 'n', 'n');
