a0a129bdbc2ff67047ba75ce2e2c7fd9e1125937
Gemma Irving <gemma.irving@ons.gov.uk>
2018-06-07 08:53:58 +0100

    Address PR comments

--- src/main/java/uk/gov/ons/ctp/response/action/domain/model/ActionType.java
+++ src/main/java/uk/gov/ons/ctp/response/action/domain/model/ActionType.java
@@ -31,16 +31,6 @@ public class ActionType implements Serializable {
   private static final long serialVersionUID = -581549382631976704L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "actiontypeseq_gen")
-  @GenericGenerator(
-          name = "actiontypeseq_gen",
-          strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-          parameters = {
-                  @org.hibernate.annotations.Parameter(name = "sequence_name", value = "action.actiontypeseq"),
-                  @org.hibernate.annotations.Parameter(name = "initial_value", value = "1000"),
-                  @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
-          }
-  )
   @Column(name = "actiontypepk")
   private Integer actionTypePK;
 
