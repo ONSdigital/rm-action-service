22f4f7dcbd6b3ab599435dc182d0e887883683d1
Ben Jefferies <benjefferies@echosoft.uk>
2018-06-14 10:44:14 +0100

    Correct action type ids

--- src/main/resources/database/changes/release-10.50.0/update_action_plan_add_social.sql
+++ src/main/resources/database/changes/release-10.50.0/update_action_plan_add_social.sql
@@ -1,5 +1,5 @@
 INSERT INTO action.actiontype
 VALUES
-(4, 'SOCIALNOT', 'Enrolment Invitation Letter', 'Printer', 'n', 'n'),
-(5, 'SOCIALREM', 'Enrolment Reminder Letter', 'Printer', 'n', 'n'),
-(6, 'SOCIALSNE', 'Survey Reminder Notification', 'Notify', 'n', 'n');
+(8, 'SOCIALNOT', 'Enrolment Invitation Letter', 'Printer', 'n', 'n'),
+(9, 'SOCIALREM', 'Enrolment Reminder Letter', 'Printer', 'n', 'n'),
+(10, 'SOCIALSNE', 'Survey Reminder Notification', 'Notify', 'n', 'n');
