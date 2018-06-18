83da0c90f3050b01b2f0385e64a380e521e8f9aa
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-06-18 13:03:09 +0100

    Add social pre notification action type
    * Adds the social pre notification type by inserting a row in the actiontype table in a liquibase change script

---
+++ src/main/resources/database/changes/release-10.50.1/update_action_type_add_social_pre_notification.sql
@@ -0,0 +1,4 @@
+INSERT INTO action.actiontype
+(actiontypepk, "name", description, handler, cancancel, responserequired)
+VALUES
+(11, 'SOCIALPRENOT', 'Social Pre Notification Letter', 'Printer', 'n', 'n')
