f6b83f645515a0b8531b9a7d2241ac9c8d0dad1a
Andrew Millar <andrew.millar@qa.com>
2018-08-20 09:23:46 +0100

    Update script to work with null fields

--- scripts/create-action-plan-selectors/4_update-action-plan-selectors.sql
+++ scripts/create-action-plan-selectors/4_update-action-plan-selectors.sql
@@ -5,15 +5,15 @@ UPDATE action.actionplan
     SET selectors = (
            CASE
              WHEN cto.sampleunittype = 'B'
-                     THEN jsonb_set(selectors, '{activeEnrolment}', '"false"', true)
+                     THEN jsonb_set(coalesce(selectors, '{}'), '{activeEnrolment}', '"false"', true)
              WHEN cto.sampleunittype = 'BI'
-                     THEN jsonb_set(selectors, '{activeEnrolment}', '"true"', true)
+                     THEN jsonb_set(coalesce(selectors, '{}'), '{activeEnrolment}', '"true"', true)
                END
            )
   FROM action.temp_cto AS cto
  WHERE id = cto.actionplanid;
 
-UPDATE action.actionplan
-   SET selectors = jsonb_set(selectors, '{collectionExerciseId}', CAST('"'||CAST(cto.collectionexerciseid AS text)||'"' AS jsonb) , true)
+UPDATE action.actionplan AS ap
+   SET selectors = jsonb_set(coalesce(ap.selectors, '{}'), '{collectionExerciseId}', CAST('"'||CAST(cto.collectionexerciseid AS text)||'"' AS jsonb) , true)
   FROM action.temp_cto AS cto
  WHERE id = cto.actionplanid;
