ca2438537d3e10952fd43b4d85fac7a26ce8b125
damien-lloyd <damien.lloyd21@gmail.com>
2018-10-01 12:25:27 +0100

    Fix for integration test: added return by date

--- src/test/java/uk/gov/ons/ctp/response/action/endpoint/ActionEndpointIT.java
+++ src/test/java/uk/gov/ons/ctp/response/action/endpoint/ActionEndpointIT.java
@@ -134,6 +134,7 @@ public class ActionEndpointIT {
     ActionAddress address = actionInstruction.getActionRequest().getAddress();
 
     checkAttributes(address);
+    assertThat(actionInstruction.getActionRequest().getReturnByDate()).isNotEmpty();
   }
 
   @Test
