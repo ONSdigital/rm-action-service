9c0e880fc1942eacb1ffce67fcedc79931952783
Amit Sinha <aks.sinha.amit@icloud.com>
2020-05-18 12:58:12 +0100

    Adding ignore

--- src/main/java/uk/gov/ons/ctp/response/action/domain/model/Action.java
+++ src/main/java/uk/gov/ons/ctp/response/action/domain/model/Action.java
@@ -21,9 +21,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.sourceforge.cobertura.CoverageIgnore;
 import uk.gov.ons.ctp.response.action.representation.ActionDTO.ActionState;
 
 /** Domain model object. */
+@CoverageIgnore
 @Entity
 @Data
 @AllArgsConstructor
