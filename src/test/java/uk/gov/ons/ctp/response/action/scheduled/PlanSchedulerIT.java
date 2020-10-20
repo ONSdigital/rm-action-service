a376abf61c22e01fb159a2de3efd4f1db3c6708e
Warren Bailey <warren@warrenbailey.net>
2020-10-20 16:03:03 +0100

    uncomment test

--- src/test/java/uk/gov/ons/ctp/response/action/scheduled/PlanSchedulerIT.java
+++ src/test/java/uk/gov/ons/ctp/response/action/scheduled/PlanSchedulerIT.java
@@ -69,7 +69,6 @@ import uk.gov.ons.ctp.response.lib.rabbit.SimpleMessageListener;
 import uk.gov.ons.ctp.response.lib.rabbit.SimpleMessageSender;
 import uk.gov.ons.ctp.response.lib.survey.representation.SurveyDTO;
 
-@Ignore
 /** Integration tests for creating action requests */
 @ContextConfiguration
 @ActiveProfiles("test")
