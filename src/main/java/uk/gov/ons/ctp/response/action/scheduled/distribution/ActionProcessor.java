498c0edb59627f223b146f1aec78cfb6b8da537c
Warren Bailey <warren@warrenbailey.net>
2020-12-08 15:48:11 +0000

    Move transactions into Service classes (#201)
    * Move transactions into Service classes
    
    * Lower transaction timeout
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    Co-authored-by: ras-rm-bot <65395820+ras-rm-bot@users.noreply.github.com>

--- src/main/java/uk/gov/ons/ctp/response/action/scheduled/distribution/ActionProcessor.java
+++ src/main/java/uk/gov/ons/ctp/response/action/scheduled/distribution/ActionProcessor.java
@@ -2,18 +2,13 @@ package uk.gov.ons.ctp.response.action.scheduled.distribution;
 
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
-import com.google.common.collect.Sets;
 import java.util.List;
-import java.util.Set;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.ons.ctp.response.action.domain.model.Action;
 import uk.gov.ons.ctp.response.action.domain.model.ActionType;
-import uk.gov.ons.ctp.response.action.domain.repository.ActionRepository;
-import uk.gov.ons.ctp.response.action.domain.repository.ActionTypeRepository;
-import uk.gov.ons.ctp.response.action.representation.ActionDTO.ActionState;
 import uk.gov.ons.ctp.response.action.service.ActionProcessingService;
+import uk.gov.ons.ctp.response.action.service.ActionService;
+import uk.gov.ons.ctp.response.action.service.ActionTypeService;
 
 /** This is the service class that processes actions */
 @Component
@@ -21,36 +16,32 @@ public class ActionProcessor {
 
   private static final Logger log = LoggerFactory.getLogger(ActionProcessor.class);
 
-  private static final int TRANSACTION_TIMEOUT_SECONDS = 3600;
-  private static final Set<ActionState> ACTION_STATES_TO_GET =
-      Sets.immutableEnumSet(ActionState.SUBMITTED, ActionState.CANCEL_SUBMITTED);
-
   public static final String NOTIFY = "Notify";
 
   public static final String PRINTER = "Printer";
 
-  private ActionRepository actionRepo;
-  private ActionTypeRepository actionTypeRepo;
+  private ActionService actionService;
+  private ActionTypeService actionTypeService;
 
   private ActionProcessingService actionProcessingService;
 
   public ActionProcessor(
-      ActionRepository actionRepo,
-      ActionTypeRepository actionTypeRepo,
+      ActionService actionService,
+      ActionTypeService actionTypeService,
       ActionProcessingService actionProcessingService) {
-    this.actionRepo = actionRepo;
-    this.actionTypeRepo = actionTypeRepo;
+    this.actionService = actionService;
+    this.actionTypeService = actionTypeService;
     this.actionProcessingService = actionProcessingService;
   }
 
   public void processEmails() {
-    List<ActionType> actionTypes = actionTypeRepo.findByHandler(NOTIFY);
+    List<ActionType> actionTypes = actionTypeService.findByHandler(NOTIFY);
     if (actionTypes.isEmpty()) {
       log.warn("no action types to process");
     }
     for (ActionType actionType : actionTypes) {
       log.with("type", actionType.getName()).info("processing actionType");
-      List<Integer> actionRules = getActionRules(actionType);
+      List<Integer> actionRules = actionService.getActionRules(actionType);
       for (Integer actionRule : actionRules) {
         log.with("actionRule", actionRule).info("processing action rule");
         processEmailsForActionRule(actionType, actionRule);
@@ -58,19 +49,11 @@ public class ActionProcessor {
     }
   }
 
-  protected List<Integer> getActionRules(ActionType actionType) {
-    return actionRepo.findDistinctActionRuleFKByActionTypeAndStateIn(
-        actionType, ACTION_STATES_TO_GET);
-  }
-
-  @Transactional(timeout = TRANSACTION_TIMEOUT_SECONDS, propagation = Propagation.REQUIRES_NEW)
   protected void processEmailsForActionRule(ActionType actionType, Integer actionRuleFK) {
     log.with("actionRule", actionRuleFK)
         .with("actionType", actionType.getName())
         .info("process emails for action rule");
-    List<Action> allActions =
-        actionRepo.findByActionTypeAndActionRuleFKAndStateIn(
-            actionType, actionRuleFK, ACTION_STATES_TO_GET);
+    List<Action> allActions = actionService.getSubmittedActions(actionType, actionRuleFK);
     if (!allActions.isEmpty()) {
       log.with("actions", allActions.size()).info("found actions to process");
       actionProcessingService.processEmails(actionType, allActions);
@@ -78,13 +61,13 @@ public class ActionProcessor {
   }
 
   public void processLetters() {
-    List<ActionType> actionTypes = actionTypeRepo.findByHandler(PRINTER);
+    List<ActionType> actionTypes = actionTypeService.findByHandler(PRINTER);
     if (actionTypes.isEmpty()) {
       log.warn("no action types to process");
     }
     for (ActionType actionType : actionTypes) {
       log.with("type", actionType.getName()).info("processing actionType");
-      List<Integer> actionRules = getActionRules(actionType);
+      List<Integer> actionRules = actionService.getActionRules(actionType);
       for (Integer actionRule : actionRules) {
         log.with("actionRule", actionRule).info("processing action rule");
         processLettersForActionRule(actionType, actionRule);
@@ -92,14 +75,11 @@ public class ActionProcessor {
     }
   }
 
-  @Transactional(timeout = TRANSACTION_TIMEOUT_SECONDS, propagation = Propagation.REQUIRES_NEW)
   protected void processLettersForActionRule(ActionType actionType, Integer actionRuleFK) {
     log.with("actionRule", actionRuleFK)
         .with("actionType", actionType.getName())
         .info("process letters for action rule");
-    List<Action> allActions =
-        actionRepo.findByActionTypeAndActionRuleFKAndStateIn(
-            actionType, actionRuleFK, ACTION_STATES_TO_GET);
+    List<Action> allActions = actionService.getSubmittedActions(actionType, actionRuleFK);
     if (!allActions.isEmpty()) {
       log.with("actions", allActions.size()).info("found actions to process");
       actionProcessingService.processLetters(actionType, allActions);
