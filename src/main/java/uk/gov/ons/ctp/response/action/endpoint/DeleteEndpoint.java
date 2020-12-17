91bf50062a5b5d09cfb56d26d43b74dbf27b65a4
Warren Bailey <warren@warrenbailey.net>
2020-12-17 15:10:39 +0000

    Reverting the latest action changes (#206)
    * Revert "Save and transitions all actions at once (#204)"
    
    This reverts commit 61469915c0cf8bcebcf67fcde4a2ceefacfdccb8.
    
    * Revert "Limit the size of the print file to 10000 (#202)"
    
    This reverts commit 4325650c792daef8626172129e7cfbfba5a5c88a.
    
    * Revert "Move transactions into Service classes (#201)"
    
    This reverts commit 0300e48bbe22b19c57a5fd2bb66266f3d8ac885d.
    
    * Revert "Transaction boundaries (#200)"
    
    This reverts commit c36cfa04dcce7a3bea10aa8288af65b46e8ff28e.
    
    * Revert "Log if no action types (#198)"
    
    This reverts commit e93593dcc825022b06d3ee9e31b7111a7aabaaa9.
    
    * Revert "Additional logging (#197)"
    
    This reverts commit e0086d28d6b6b82360d50fedf717f2fc8a4eecc0.
    
    * Revert "Process each action rule individually and in their own transaction (#194)"
    
    This reverts commit 264ce8e46e6e6305961670867a3a4e3b7a6ea261.
    
    * Revert "Separate cronjobs for emails and letters (#193)"
    
    This reverts commit b9f36b20f6402b68d995f894481eb01f1259cd61.
    
    * Revert "Action simplification and final action exporter integration  (#191)"
    
    This reverts commit 4fbc5ffeafcda14bff151fd412bd87ac7530c4f1.
    
    * go to version 12.2.0
    
    * increase helm chart version

---
+++ src/main/java/uk/gov/ons/ctp/response/action/endpoint/DeleteEndpoint.java
@@ -0,0 +1,42 @@
+package uk.gov.ons.ctp.response.action.endpoint;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.ons.ctp.response.action.domain.model.ExportJob;
+import uk.gov.ons.ctp.response.action.scheduled.export.DeleteProcessor;
+import uk.gov.ons.ctp.response.lib.common.error.CTPException;
+
+@RestController
+@RequestMapping(value = "/delete-old-data", produces = "application/json")
+public class DeleteEndpoint {
+  private static final Logger log = LoggerFactory.getLogger(DeleteEndpoint.class);
+
+  @Autowired private DeleteProcessor deleteProcessor;
+
+  @RequestMapping(method = RequestMethod.DELETE)
+  public ResponseEntity<String> triggerDelete() throws CTPException {
+    try {
+      log.info("Beginning deletion of old records");
+      List<ExportJob> exportJobs = deleteProcessor.getAllExportJobIdsForDeletion();
+
+      for (ExportJob exportJob : exportJobs) {
+        deleteProcessor.triggerDeleteForExportJob(exportJob);
+      }
+      log.info("Completed deletion of old records. [" + exportJobs.size() + "] exportJobs deleted");
+      return ResponseEntity.ok()
+          .body(
+              "Completed deletion of old records. [" + exportJobs.size() + "] exportJobs deleted");
+    } catch (RuntimeException e) {
+      log.error(
+          "Uncaught exception - transaction rolled back. Will re-run when scheduled by cron", e);
+      throw new CTPException(
+          CTPException.Fault.SYSTEM_ERROR, "Uncaught exception when deleting old records");
+    }
+  }
+}
