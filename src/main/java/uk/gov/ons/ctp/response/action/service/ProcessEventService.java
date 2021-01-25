c11ab7c4b04d35331f98c93261dddb20324d7234
Amit Sinha <aks.sinha.amit@icloud.com>
2021-01-25 17:03:01 +0000

    updates

--- src/main/java/uk/gov/ons/ctp/response/action/service/ProcessEventService.java
+++ src/main/java/uk/gov/ons/ctp/response/action/service/ProcessEventService.java
@@ -102,6 +102,7 @@ public class ProcessEventService {
     Instant instant = Instant.now();
     CollectionExerciseDTO collectionExercise = getCollectionExercise(collectionExerciseId);
     SurveyDTO survey = getSurvey(collectionExercise.getSurveyId());
+    partialProcessCheckPoint(collectionExerciseId, eventTag, partialEntry, instant);
     log.debug("Getting Email cases against collectionExerciseId and event active enrolment");
     List<ActionCase> emailCases =
         actionCaseRepository.findByCollectionExerciseIdAndActiveEnrolment(
@@ -143,7 +144,6 @@ public class ProcessEventService {
           .with("event", eventTag)
           .info("No Letters to process");
     }
-    partialProcessCheckPoint(collectionExerciseId, eventTag, partialEntry, instant);
   }
 
   private void partialProcessCheckPoint(
