a133c54eca69173940aa6c00e7ba346f6112276a
Adam Wilkie <Adam.Wilkie@ons.gov.uk>
2021-05-12 09:22:41 +0100

    reformatting sql code

--- src/main/resources/database/changes/release-29/mofiying_column_names.sql
+++ src/main/resources/database/changes/release-29/mofiying_column_names.sql
@@ -1,14 +1,12 @@
-ALTER TABLE action.case
-RENAME COLUMN actionplanid TO action_plan_id
-RENAME COLUMN actionplanstartdate TO action_plan_start_date
-RENAME COLUMN actionplanfk to action_plan_fk
-RENAME COLUMN actionplanenddate TO action_plan_end_date
-RENAME COLUMN partyid TO party_id
-RENAME COLUMN collectionexerciseid TO collection_exercise_id
-RENAME COLUMN sampleunittype TO sample_unit_type
-RENAME COLUMN casepk TO case_pk;
+ALTER TABLE action.case RENAME COLUMN actionplanid TO action_plan_id;
+ALTER TABLE action.case RENAME COLUMN actionplanstartdate TO action_plan_start_date;
+ALTER TABLE action.case RENAME COLUMN actionplanfk to action_plan_fk;
+ALTER TABLE action.case RENAME COLUMN actionplanenddate TO action_plan_end_date;
+ALTER TABLE action.case RENAME COLUMN partyid TO party_id;
+ALTER TABLE action.case RENAME COLUMN collectionexerciseid TO collection_exercise_id;
+ALTER TABLE action.case RENAME COLUMN sampleunittype TO sample_unit_type;
+ALTER TABLE action.case RENAME COLUMN casepk TO case_pk;
 
-ALTER TABLE action.actionplan
-RENAME COLUMN actionplanpk TO action_plan_pk
-RENAME COLUMN createdby TO created_by
-RENAME COLUMN lastrundatetime TO last_run_date_time;
+ALTER TABLE action.actionplan RENAME COLUMN actionplanpk TO action_plan_pk;
+ALTER TABLE action.actionplan RENAME COLUMN createdby TO created_by;
+ALTER TABLE action.actionplan RENAME COLUMN lastrundatetime TO last_run_date_time;
