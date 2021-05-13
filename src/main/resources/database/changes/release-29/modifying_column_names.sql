32222c2622cf81fc56ed7235d9cfadd2ab7ca38f
anwilkie <47449873+anwilkie@users.noreply.github.com>
2021-05-13 14:06:03 +0100

    Updating schema column names (#237)
    * creating database migration
    
    * updated column names in java class
    
    * incrementing chart version number
    
    * auto patch increment
    
    * undoing changes to liquibase tables
    
    * reformatting sql code
    
    * fixing filename
    
    * added missing underscore to sample_unit_id
    
    Co-authored-by: ras-rm-pr-bot <matt.crocker+rasrm@gmail.com>

---
+++ src/main/resources/database/changes/release-29/modifying_column_names.sql
@@ -0,0 +1,13 @@
+ALTER TABLE action.case RENAME COLUMN actionplanid TO action_plan_id;
+ALTER TABLE action.case RENAME COLUMN actionplanstartdate TO action_plan_start_date;
+ALTER TABLE action.case RENAME COLUMN actionplanfk to action_plan_fk;
+ALTER TABLE action.case RENAME COLUMN actionplanenddate TO action_plan_end_date;
+ALTER TABLE action.case RENAME COLUMN partyid TO party_id;
+ALTER TABLE action.case RENAME COLUMN collectionexerciseid TO collection_exercise_id;
+ALTER TABLE action.case RENAME COLUMN sampleunittype TO sample_unit_type;
+ALTER TABLE action.case RENAME COLUMN casepk TO case_pk;
+ALTER TABLE action.case RENAME COLUMN sampleunit_id TO sample_unit_id;
+
+ALTER TABLE action.actionplan RENAME COLUMN actionplanpk TO action_plan_pk;
+ALTER TABLE action.actionplan RENAME COLUMN createdby TO created_by;
+ALTER TABLE action.actionplan RENAME COLUMN lastrundatetime TO last_run_date_time;
