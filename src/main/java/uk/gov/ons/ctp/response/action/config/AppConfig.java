5d3d791ec5ee6f01e528cb0b6aa1d548447e0e0b
Warren Bailey <warren@warrenbailey.net>
2021-09-17 09:16:28 +0100

    Removing all traces of rabbit (#251)
    * Removing all traces of rabbit
    
    * auto patch increment
    
    Co-authored-by: ras-rm-pr-bot <matt.crocker+rasrm@gmail.com>

--- src/main/java/uk/gov/ons/ctp/response/action/config/AppConfig.java
+++ src/main/java/uk/gov/ons/ctp/response/action/config/AppConfig.java
@@ -5,7 +5,6 @@ import net.sourceforge.cobertura.CoverageIgnore;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
-import uk.gov.ons.ctp.response.lib.rabbit.Rabbitmq;
 
 /** The apps main holder for centralized config read from application.yml or env vars */
 @CoverageIgnore
@@ -20,7 +19,6 @@ public class AppConfig {
   private SurveySvc surveySvc;
   private DataGrid dataGrid;
   private ReportSettings reportSettings;
-  private Rabbitmq rabbitmq;
   private Logging logging;
   private GCP gcp;
 }
