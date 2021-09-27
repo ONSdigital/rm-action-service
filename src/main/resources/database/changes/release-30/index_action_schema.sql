0a14f128baf402e35e4a76a6ce62da649b0d48c7
David Morgan <davidmorgan@ext.ons.gov.uk>
2021-09-27 15:25:43 +0100

    added hypen

--- src/main/resources/database/changes/release-30/index_action_schema.sql
+++ src/main/resources/database/changes/release-30/index_action_schema.sql
@@ -8,7 +8,7 @@
 -- Index: action_eventcase_id_index
 -- DROP INDEX action_eventcase_id_index;
 
-CREATE INDEX action_eventcase_id_index ON action.action_event USING btree (case_id);
+CREATE INDEX action_event_case_id_index ON action.action_event USING btree (case_id);
 
 
 -- Index: action_eventtype_index
