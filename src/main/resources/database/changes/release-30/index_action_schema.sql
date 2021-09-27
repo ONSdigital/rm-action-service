4b5bcb619e0134838bb7b04cd735b6514c1d0e41
David Morgan <davidmorgan@ext.ons.gov.uk>
2021-09-27 15:57:17 +0100

    added hypen

--- src/main/resources/database/changes/release-30/index_action_schema.sql
+++ src/main/resources/database/changes/release-30/index_action_schema.sql
@@ -5,32 +5,27 @@
 -- action_event table
 
 
--- Index: action_eventcase_id_index
--- DROP INDEX action_eventcase_id_index;
+-- Index: action_event_case_id_index
 
 CREATE INDEX action_event_case_id_index ON action.action_event USING btree (case_id);
 
 
--- Index: action_eventtype_index
--- DROP INDEX action_eventtype_index;
+-- Index: action_event_type_index
 
-CREATE INDEX action_eventtype_index ON action.action_event USING btree (type);
+CREATE INDEX action_event_type_index ON action.action_event USING btree (type);
 
 
--- Index: action_eventhandler_index
--- DROP INDEX action_eventhandler_index;
+-- Index: action_event_handler_index
 
-CREATE INDEX action_eventhandler_index ON action.action_event USING btree (handler);
+CREATE INDEX action_event_handler_index ON action.action_event USING btree (handler);
 
 
 -- Index: action_eventevent_tag_index
--- DROP INDEX action_eventevent_tag_index;
 
-CREATE INDEX action_eventevent_tag_index ON action.action_event USING btree (event_tag);
+CREATE INDEX action_event_event_tag_index ON action.action_event USING btree (event_tag);
 
 
--- Index: action_eventstatus_index
--- DROP INDEX action_eventstatus_index;
+-- Index: action_event_status_index
 
-CREATE INDEX action_eventstatus_index ON action.action_event USING btree (status);
+CREATE INDEX action_event_status_index ON action.action_event USING btree (status);
 
