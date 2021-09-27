0f03e422db053276cd9bc2fc83c02395470bbb4d
David Morgan <davidmorgan@ext.ons.gov.uk>
2021-09-27 12:49:51 +0100

    add new indexes on action.event

---
+++ src/main/resources/database/changes/release-30/index_action_schema.sql
@@ -0,0 +1,36 @@
+-- ACTION SERVICE
+
+
+
+-- action_event table
+
+
+-- Index: action_eventcase_id_index
+-- DROP INDEX action_eventcase_id_index;
+
+CREATE INDEX action_eventcase_id_index ON action.action_event USING btree (case_id);
+
+
+-- Index: action_eventtype_index
+-- DROP INDEX action_eventtype_index;
+
+CREATE INDEX action_eventtype_index ON action.action_event USING btree (type);
+
+
+-- Index: action_eventhandler_index
+-- DROP INDEX action_eventhandler_index;
+
+CREATE INDEX action_eventhandler_index ON action.action_event USING btree (handler);
+
+
+-- Index: action_eventevent_tag_index
+-- DROP INDEX action_eventevent_tag_index;
+
+CREATE INDEX action_eventevent_tag_index ON action.action_event USING btree (event_tag);
+
+
+-- Index: action_eventstatus_index
+-- DROP INDEX action_eventstatus_index;
+
+CREATE INDEX action_eventstatus_index ON action.action_event USING btree (status);
+
